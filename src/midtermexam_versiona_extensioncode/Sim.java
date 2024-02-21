325861a7f1c490d457660f5784ce2a5f572f9787
Sim Yuan Hee <yuanhee@sheridancollege.ca>
2024-02-21 10:04:49 -0500

    new branch and new class named "Sim"

---
+++ src/midtermexam_versiona_extensioncode/Sim.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author Sim Yuan Hee
+ * February 21, 2024
+ */
+public class Sim {
+    
+}
