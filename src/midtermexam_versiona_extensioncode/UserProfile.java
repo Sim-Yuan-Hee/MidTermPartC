a06c1071d47773587be40262236a10716b67a51a
Sim-Yuan-Hee <156929935+Sim-Yuan-Hee@users.noreply.github.com>
2024-02-21 10:10:26 -0500

    Update UserProfile.java

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -12,6 +12,7 @@ package midtermexam_versiona_extensioncode;
  * Students are reminded to add themselves as modifiers when editing the
  * code.
  * @author dancye, 2019
+ * Sim Yuan Hee, February 21 2024
  */
 public class UserProfile 
 {
